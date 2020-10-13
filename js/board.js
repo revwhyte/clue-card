9000d848fa4bcc135ea2ccd61fa21561eba96c62
Philippe Correia <revwhyte@gmail.com>
2020-10-13 10:24:55 -0300

    insert message

--- js/board.js
+++ js/board.js
@@ -2,3 +2,20 @@
 function doSomething() {
     return true;
 }
+
+function setName(id) {
+    let nome = prompt('Enter player\'s name')
+    if(nome == '') {
+        alert('name not informed')
+        return false
+    }
+
+    let names = document.querySelectorAll('.pname').forEach(el => {
+        if(el.innerText == 1) {
+            alert('name already used')
+            return false
+        }
+    })
+
+    $(`#${id}`).text(nome)
+}
